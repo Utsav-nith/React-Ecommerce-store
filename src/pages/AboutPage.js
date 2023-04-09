e0c7c8413bb892ebf8c22402447b6b68ec529b60
Utsav <78713120+Utsav-nith@users.noreply.github.com>
2023-04-10 03:44:34 +0530

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpg'
+import aboutImg from '../assets/hero.jpg'
 const AboutPage = () => {
   return (
     <main>
